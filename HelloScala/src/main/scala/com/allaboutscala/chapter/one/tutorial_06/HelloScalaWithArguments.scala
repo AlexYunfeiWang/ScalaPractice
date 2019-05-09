e22940eaaddbe72e0b0971a2b751a322e033119c
Valkyrie <wyfwork99@gmail.com>
2019-05-09 19:37:26 -0400

    command line args print

--- HelloScala/src/main/scala/com/allaboutscala/chapter/one/tutorial_06/HelloScalaWithArguments.scala
+++ HelloScala/src/main/scala/com/allaboutscala/chapter/one/tutorial_06/HelloScalaWithArguments.scala
@@ -8,6 +8,12 @@ command line args in Scala
 object HelloScalaWithArguments extends App{
   println("Hello Scala With Arguments Scala Application!");
 
+  //the args are: "1st Argument" 2nd Argument 3rdArgument
   println("Command line arguments are: ");
-  println(args.mkString(": "));
+
+  //print 2nd and 3rd args
+  println(args(1) + " " + args(2));
+
+  //join args with comma, then print
+  println(args.mkString(", "));
 }
