c243fc33e27a067de1d7619be23ac2bdd74a06df
Valkyrie <wyfwork99@gmail.com>
2019-05-09 19:12:18 -0400

    practice scala main function

---
+++ HelloScala/src/main/scala/com/allaboutscala/chapter/one/tutorial_05/HelloScalaMain.scala
@@ -0,0 +1,16 @@
+package com.allaboutscala.chapter.one.tutorial_05
+
+/*
+Created by Alex Yunfei Wang on 05/09/2019
+This is an example of using scala main function
+
+def keyword it used to define a function
+:Unit = in Scala, the Unit keyword is used to
+define a function which does not return anything, this
+is similar to void keyword in Java
+ */
+object HelloScalaMain {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala from Main Function!");
+  }
+}
