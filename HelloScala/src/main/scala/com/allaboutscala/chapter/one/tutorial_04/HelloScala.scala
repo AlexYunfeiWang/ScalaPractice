65eba75bf4c5a247f88192c9b0ab9a419c2d83b4
Valkyrie <wyfwork99@gmail.com>
2019-05-09 19:01:14 -0400

    My first Scala program

--- HelloScala/src/main/scala/com/allaboutscala/chapter/one/tutorial_04/HelloScala.scala
+++ HelloScala/src/main/scala/com/allaboutscala/chapter/one/tutorial_04/HelloScala.scala
@@ -4,6 +4,6 @@ package com.allaboutscala.chapter.one.tutorial_04
 Created by Alex Yunfei Wang on 05/09/2019
  */
 
-object HelloScala {
+object HelloScala extends App{
   println("Hello World from Scala!");
 }
