23fc07e60662c1aea2d47b0c7961d72ec72ec74c
Valkyrie <wyfwork99@gmail.com>
2019-05-09 20:06:26 -0400

    debug scala application

---
+++ HelloScala/src/main/scala/com/allaboutscala/chapter/one/tutorial_07/HelloScalaWithArgumentsDebug.scala
@@ -0,0 +1,14 @@
+package com.allaboutscala.chapter.one.tutorial_07
+
+/*
+This program shows how to debug scala program
+ */
+
+object HelloScalaWithArgumentsDebug extends App{
+
+  println("Hello World With Arguments Scala Application!");
+
+  println("Command line arguments are: ");
+  println(args.mkString(", "));
+
+}
