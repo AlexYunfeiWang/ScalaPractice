6dccd09265ec9a0c7ad9ff5ae927cbf511625e01
AlexYunfeiWang <wyfwork99@gmail.com>
2019-05-26 23:33:03 -0700

    FunctionWithImplicitParams

---
+++ FunctionWithImplicitParams/src/main/scala/FunctionWithImplicitParams.scala
@@ -0,0 +1,35 @@
+object FunctionWithImplicitParams {
+
+  def main (args: Array[String]): Unit = {
+    println(s"Step 1: How to define a function with an implicit parameter")
+    def totalCost(donutType: String, quantity: Int)(implicit discount: Double): Double = {
+      println(s"Calculating the price for $quantity $donutType")
+      val totalCost = 2.50 * quantity * (1 - discount)
+      totalCost
+    }
+
+    println("\nStep 2: How to define an implicit value")
+    implicit val discount: Double = 0.2
+    println(s"All customer will receive a ${discount * 100}% discount")
+
+    println("\nStep 3: How to call a function which has an implicit parameter")
+    println(s"""Total cost with discount of 5 Glazed Donuts = ${totalCost("Glazed Donut", 5)}""")
+
+    println("\nStep 4: How to define a function which takes multiple implicit parameters")
+    def totalCost2(donutType: String, quantity: Int)(implicit discount: Double, storeName: String): Double = {
+      println(s"[$storeName] Calculating the price for $quantity $donutType")
+      val totalCost = 2.50 * quantity * (1 - discount)
+      totalCost
+    }
+    println("\nStep 5: How to call a function which takes multiple implicit parameters")
+    implicit val storeName: String = "Tasty Donut Store"
+    println(s"""Total cost with discount of 5 Glazed Donuts = ${totalCost2("Glazed Donut", 5)}""")
+
+    println("\nStep 6: How to manually pass-through implicit parameters")
+    println(
+      s"Total cost with discount of 5 " +
+        s"Glazed Donuts, manually passed-through = " +
+        s"${totalCost2("Glazed Donut", 5)(0.1, "Scala Donut Store")}")
+  }
+}
+
